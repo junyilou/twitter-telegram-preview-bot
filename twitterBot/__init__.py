63dd6225de25610134c1e14175ee782648545da8
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2024-12-31 23:54:47 +0800

    fix: remove cookies requirement

--- twitterBot/__init__.py
+++ twitterBot/__init__.py
@@ -1,10 +1,7 @@
-from .group import command_handler, group_handlers
-from .inline import inline_handler
+from telegram.ext import BaseHandler
 
-handler_group = [inline_handler, command_handler, *group_handlers]
-
-try:
-	from modules.tweet import get_scrapper
-	get_scrapper()
-except FileNotFoundError:
-	raise FileNotFoundError("Please deploy bot-scraper.cookies first") from None
+def load() -> list[BaseHandler]:
+	from .group import command_handler, group_handlers
+	from .inline import inline_handler
+	handler_group = [inline_handler, command_handler, *group_handlers]
+	return handler_group

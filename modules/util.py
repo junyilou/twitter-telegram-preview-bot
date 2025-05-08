9991a5f6cd1f92c032a454b41ffeb09a8eed7461
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2025-05-08 13:12:58 +0800

    kw_only disMarkdown

--- modules/util.py
+++ modules/util.py
@@ -6,9 +6,9 @@ import aiohttp
 browser_agent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
 	"AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15"}
 
-def disMarkdown(text: str, wrap: str = "", extra: str = "") -> str:
-	signs = "\\|_{}[]()#@+-.!=<>~" + extra
-	text = text.translate({ord(s): f"\\{s}" for s in signs})
+def disMarkdown(text: str, *, wrap: str = "", extra: str = "", remove: str = "") -> str:
+	signs = "\\|_{}[]()#@+-.!=<>~`" + extra
+	text = text.translate({ord(s): f"\\{s}" for s in signs if s not in remove})
 	return wrap + text + wrap[::-1]
 
 def time_delta(*,
